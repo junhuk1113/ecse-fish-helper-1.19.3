11b0d6acd00e43a67911722af59e550a8041d7b5
junhyeok <junhuk1113@naver.com>
2023-06-21 23:26:58 +0900

    first commit

---
+++ src/main/java/net/pmkjun/ecsefishhelper/FishHelperMod.java
@@ -0,0 +1,20 @@
+package net.pmkjun.ecsefishhelper;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FishHelperMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+
+
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
---
+++ src/main/java/net/pmkjun/ecsefishhelper/mixin/ItemPickupMixin.java
@@ -0,0 +1,37 @@
+package net.pmkjun.ecsefishhelper.mixin;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.SlotActionType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ScreenHandler.class)
+public class ItemPickupMixin {
+	private static final Logger LOGGER = LogManager.getLogger("ItemPickupMixin");
+	@Shadow
+	private ItemStack cursorStack;
+	@Inject(method = "onSlotClick", at = @At("RETURN"))
+	private void onSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+		if (player instanceof ClientPlayerEntity) {
+			ClientPlayerEntity clientPlayer = (ClientPlayerEntity) player;
+			if (!cursorStack.isEmpty()&&!cursorStack.hasNbt()){
+				LOGGER.info("dont have nbt");
+			}
+			if (!cursorStack.isEmpty()&&cursorStack.hasNbt()) {
+				//Text itemName = cursorStack.getName();
+				//LOGGER.info("item.pickup: " + itemName.getString());
+
+				LOGGER.info("item.pickup: " + cursorStack.getName().getString());
+				//LOGGER.info("item.pickup: " + cursorStack.getTooltipData().);
+			}
+		}
+	}
+}
