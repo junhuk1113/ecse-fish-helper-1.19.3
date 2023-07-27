88e564b9cce3a1e737ed0ae68ff91dfb6c7dd9f5
junhyeok <junhuk1113@naver.com>
2023-07-27 17:04:25 +0900

    다른사람 낚시찌 음소거 테스트 코드2

--- src/main/java/net/pmkjun/ecsefishhelper/mixin/FishingBobberSilentMixin.java
+++ src/main/java/net/pmkjun/ecsefishhelper/mixin/FishingBobberSilentMixin.java
@@ -1,11 +1,18 @@
 package net.pmkjun.ecsefishhelper.mixin;
 
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -17,14 +24,23 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(FishingBobberEntity.class)
 public class FishingBobberSilentMixin extends ProjectileEntity {
-
     @Shadow
     @Final
     private static TrackedData<Integer> HOOK_ENTITY_ID;
-
     @Shadow
     @Final
     private static TrackedData<Boolean> CAUGHT_FISH;
+    @Shadow
+    private int hookCountdown;
+    @Shadow
+    private int waitCountdown;
+    @Shadow
+    private int fishTravelCountdown;
+    @Shadow
+    private float fishAngle;
+    @Shadow
+    @Final
+    private int lureLevel;
 
     public FishingBobberSilentMixin(EntityType<? extends ProjectileEntity> entityType, World world) {
         super(entityType, world);
@@ -37,13 +53,84 @@ public class FishingBobberSilentMixin extends ProjectileEntity {
     }
 
 
-    @Inject(method = "setOwner(Lnet/minecraft/entity/Entity;)V", at = @At("RETURN"))
-    private void setSlientMixin(@Nullable Entity entity, CallbackInfo ci){
-        System.out.println("setOwner called");
-        if(entity != null){
-            entity.setSilent(true);
-            System.out.println("isSilent : "+entity.isSilent());
+    @Inject(method = "tickFishingLogic(Lnet/minecraft/util/math/BlockPos;)V", at = @At("HEAD"))
+    private void tickFishingMixin(BlockPos pos, CallbackInfo cir){
+        System.out.println("tickFishingLogic Called");
+        ServerWorld serverWorld = (ServerWorld)this.world;
+        int i = 1;
+        BlockPos blockPos = pos.up();
+        if (this.random.nextFloat() < 0.25f && this.world.hasRain(blockPos)) {
+            ++i;
         }
-        else System.out.println("entity is null!");
+        if (this.random.nextFloat() < 0.5f && !this.world.isSkyVisible(blockPos)) {
+            --i;
+        }
+        if (this.hookCountdown > 0) {
+            --this.hookCountdown;
+            if (this.hookCountdown <= 0) {
+                this.waitCountdown = 0;
+                this.fishTravelCountdown = 0;
+                this.getDataTracker().set(CAUGHT_FISH, false);
+            }
+        } else if (this.fishTravelCountdown > 0) {
+            this.fishTravelCountdown -= i;
+            if (this.fishTravelCountdown > 0) {
+                double j;
+                double e;
+                this.fishAngle += (float)this.random.nextTriangular(0.0, 9.188);
+                float f = this.fishAngle * ((float)Math.PI / 180);
+                float g = MathHelper.sin(f);
+                float h = MathHelper.cos(f);
+                double d = this.getX() + (double)(g * (float)this.fishTravelCountdown * 0.1f);
+                BlockState blockState = serverWorld.getBlockState(new BlockPos(d, (e = (double)((float)MathHelper.floor(this.getY()) + 1.0f)) - 1.0, j = this.getZ() + (double)(h * (float)this.fishTravelCountdown * 0.1f)));
+                if (blockState.isOf(Blocks.WATER)) {
+                    if (this.random.nextFloat() < 0.15f) {
+                        serverWorld.spawnParticles(ParticleTypes.BUBBLE, d, e - (double)0.1f, j, 1, g, 0.1, h, 0.0);
+                    }
+                    float k = g * 0.04f;
+                    float l = h * 0.04f;
+                    serverWorld.spawnParticles(ParticleTypes.FISHING, d, e, j, 0, l, 0.01, -k, 1.0);
+                    serverWorld.spawnParticles(ParticleTypes.FISHING, d, e, j, 0, -l, 0.01, k, 1.0);
+                }
+            } else {
+                System.out.println("playSound muted");
+                //this.playSound(SoundEvents.ENTITY_FISHING_BOBBER_SPLASH, 0.25f, 1.0f + (this.random.nextFloat() - this.random.nextFloat()) * 0.4f);
+                double m = this.getY() + 0.5;
+                serverWorld.spawnParticles(ParticleTypes.BUBBLE, this.getX(), m, this.getZ(), (int)(1.0f + this.getWidth() * 20.0f), this.getWidth(), 0.0, this.getWidth(), 0.2f);
+                serverWorld.spawnParticles(ParticleTypes.FISHING, this.getX(), m, this.getZ(), (int)(1.0f + this.getWidth() * 20.0f), this.getWidth(), 0.0, this.getWidth(), 0.2f);
+                this.hookCountdown = MathHelper.nextInt(this.random, 20, 40);
+                this.getDataTracker().set(CAUGHT_FISH, true);
+            }
+        } else if (this.waitCountdown > 0) {
+            this.waitCountdown -= i;
+            float f = 0.15f;
+            if (this.waitCountdown < 20) {
+                f += (float)(20 - this.waitCountdown) * 0.05f;
+            } else if (this.waitCountdown < 40) {
+                f += (float)(40 - this.waitCountdown) * 0.02f;
+            } else if (this.waitCountdown < 60) {
+                f += (float)(60 - this.waitCountdown) * 0.01f;
+            }
+            if (this.random.nextFloat() < f) {
+                double j;
+                double e;
+                float g = MathHelper.nextFloat(this.random, 0.0f, 360.0f) * ((float)Math.PI / 180);
+                float h = MathHelper.nextFloat(this.random, 25.0f, 60.0f);
+                double d = this.getX() + (double)(MathHelper.sin(g) * h) * 0.1;
+                BlockState blockState = serverWorld.getBlockState(new BlockPos(d, (e = (double)((float)MathHelper.floor(this.getY()) + 1.0f)) - 1.0, j = this.getZ() + (double)(MathHelper.cos(g) * h) * 0.1));
+                if (blockState.isOf(Blocks.WATER)) {
+                    serverWorld.spawnParticles(ParticleTypes.SPLASH, d, e, j, 2 + this.random.nextInt(2), 0.1f, 0.0, 0.1f, 0.0);
+                }
+            }
+            if (this.waitCountdown <= 0) {
+                this.fishAngle = MathHelper.nextFloat(this.random, 0.0f, 360.0f);
+                this.fishTravelCountdown = MathHelper.nextInt(this.random, 20, 80);
+            }
+        } else {
+            this.waitCountdown = MathHelper.nextInt(this.random, 100, 600);
+            this.waitCountdown -= this.lureLevel * 20 * 5;
+        }
+
     }
+
 }
